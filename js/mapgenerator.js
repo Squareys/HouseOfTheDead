9a0258e69bcd89a21654cd9356f0c7ceac286981
Timmy Kokke <mental_c@hotmail.com>
2021-09-30 10:57:21 +0200

    refactoring

--- js/mapgenerator.js
+++ js/mapgenerator.js
@@ -1,50 +1,9 @@
 //import * as WL from "@wonderlandengine/api"
+import { leveldata } from './data/levelData';
 import { wlUtils } from './wlUtils';
 
 // console.log(WL);
-const leveldata = {
-    rooms: [
-        // [
-        //     [{ orientation: 'N', wall: 'WC', floor: 'W', ceiling: 'W' }, { orientation: 'N', wall: 'A', floor: 'W', ceiling: 'W' }, { orientation: 'E', wall: 'WC', floor: 'W', ceiling: 'W' }],
-        //     [{ orientation: 'W', wall: 'A', floor: 'W', ceiling: 'W' }, { floor: 'W', ceiling: 'W', light: '' }, { orientation: 'E', wall: 'W', floor: 'W', ceiling: 'W' }],
-        //     [{ orientation: 'W', wall: 'WC', floor: 'W', ceiling: 'W' }, { orientation: 'S', wall: 'A', floor: 'W', ceiling: 'W' }, { orientation: 'S', wall: 'WC', floor: 'W', ceiling: 'W' }]
-        // ],
-        [
-            [{ orientation: 'N', wall: ['WC', 'WCWI'], floor: 'W', ceiling: 'W' }, { orientation: 'N', wall: ['WI', 'W', 'A'], prop: 'prop-books', floor: 'W', ceiling: 'W' }, { orientation: 'E', wall: 'WC', floor: 'W', ceiling: 'W' }],
-            [{ orientation: 'W', wall: 'A', floor: 'W', ceiling: 'W',prop: 'prop-bed-single'}, { floor: 'W', ceiling: 'W', light: '' }, { orientation: 'E', wall: ['WI', 'W', 'A'], floor: 'W', ceiling: 'W' }],
-            [{ orientation: 'W', wall: ['WC', 'WCWI'], floor: 'W', ceiling: 'W',prop: 'prop-chair1'}, { orientation: 'S', wall: ['WI', 'W'], floor: 'W', ceiling: 'W',prop: 'prop-small-table' }, { orientation: 'S', wall: 'WC', floor: 'W', ceiling: 'W',prop: 'prop-chair2' }]
-        ],
-        [
-            [{ orientation: 'N', wall: ['WCA', 'WC', 'WCWI'], floor: 'W', ceiling: 'W' }, { orientation: 'E', wall: ['WC', 'WCWI'], floor: 'W', ceiling: 'W' }],
-            [{ orientation: 'W', wall: ['WC', 'WCWI'], floor: 'W', ceiling: 'W' }, { orientation: 'S', wall: ['WC', 'WCA'], floor: 'W', ceiling: 'W' }]
-        ],
-        [
-            [{ orientation: 'W', wall: ['WE', 'WEA'], floor: 'W', ceiling: 'W' }, { floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA'], orientation: ['N', 'S'] }, { orientation: 'E', wall: ['WE', 'WEA'], floor: 'W', ceiling: 'W' }],
-        ],
-        [
-            [{ orientation: 'W', wall: ['WE', 'WEA'], floor: 'W', ceiling: 'W' }, { floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['N', 'S'] }, { floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['N', 'S'] }, { floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['N', 'S'] }, { orientation: 'E', wall: ['WE', 'WEA'], floor: 'W', ceiling: 'W' }],
-        ],
-        [
-            [{ orientation: 'N', wall: ['WEA', 'WE', 'WEWI'], floor: 'W', ceiling: 'W' }, {}],
-            [{ orientation: 'W', wall: ['WC', 'WCWI'], floor: 'W', ceiling: 'W' }, { orientation: 'E', wall: ['WEA', 'WE', 'WEWI'], floor: 'W', ceiling: 'W' }],
-        ],
-        [
-            [{ orientation: 'N', wall: ['WE', 'WEA', 'WEWI'], floor: 'W', ceiling: 'W' }],
-            [{ floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA'], orientation: ['E', 'W'] }],
-            [{ orientation: 'S', wall: ['WE', 'WEA', 'WEWI'], floor: 'W', ceiling: 'W' }],
-        ],
-        [
-            [{ orientation: 'N', wall: ['WE', 'WEA', 'WEWI'], floor: 'W', ceiling: 'W' }],
-            [{ floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['E', 'W'] }],
-            [{ floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['E', 'W'] }],
-            [{ floor: 'W', ceiling: 'W', wall: ['WSA', 'WWSA', 'WS', 'WSWI'], orientation: ['E', 'W'] }],
-            [{ orientation: 'S', wall: ['WE', 'WEA', 'WEWI'], floor: 'W', ceiling: 'W' }],
-        ],
-        [
-            [{ orientation: 'N', wall: ['WBSA', 'WBSAWI'], floor: 'W', ceiling: 'W' }]
-        ],
-    ]
-}
+
 
 /**
  * @type {WL.Component}
@@ -88,6 +47,9 @@ const mapgenComponent = {
     // store the location of the exits of the room.
     // if the room fits, add it to the level.
     // run addRoom for every exit.   
+    // TODO:
+    // - find places to connect rooms together, check if there are doors on 1 side, and if so, connect them.
+    // - 
     addRoom(startingExit) {
         let roomFound = false;
         let numTries = 60;
